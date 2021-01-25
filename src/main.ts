f232b10eef6345b0b5b29c2dd91de0d90668215c
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-01-26 01:34:30 +0200

    add a simple proof-of-concept implementation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,57 @@
-console.log('hello world');
+import * as discord from 'discord.js';
+import * as argparse from 'argparse';
+import * as utils from './utils';
+
+async function main(): Promise<void> {
+  let parser = new argparse.ArgumentParser();
+  parser.add_argument('--guild-id', {
+    required: true,
+  });
+  parser.add_argument('--bot-token', {
+    required: true,
+  });
+  let cliArgs = parser.parse_args();
+
+  let client = new discord.Client();
+
+  client.on('ready', async () => {
+    try {
+      console.warn(`Logged in as ${client.user!.tag}`);
+
+      let guild = await client.guilds.fetch(cliArgs.guild_id);
+      let textChannels = guild.channels.cache.filter(
+        (c) => c instanceof discord.TextChannel && c.viewable,
+      );
+
+      let collectedMessages = 0;
+      let interval = setInterval(() => {
+        console.warn(new Date().toISOString(), `Collected ${collectedMessages} messages`);
+      }, 3000).unref();
+
+      for (let channel of textChannels.values()) {
+        let options: discord.ChannelLogsQueryOptions = { limit: 100 };
+        while (true) {
+          let messages = await (channel as discord.TextChannel).messages.fetch(options);
+          if (messages.size === 0) break;
+
+          for (let msg of messages.values()) {
+            options.before = msg.id;
+            console.log(JSON.stringify(msg.toJSON()));
+            collectedMessages++;
+          }
+
+          // rate limit is 5 requests per 5 seconds
+          await utils.wait(1000);
+        }
+      }
+
+      clearInterval(interval);
+    } finally {
+      client.destroy();
+    }
+  });
+
+  await client.login(cliArgs.bot_token);
+}
+
+void main();
